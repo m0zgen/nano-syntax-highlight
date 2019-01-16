b8c637fe5a6f6ab1b7f6d0ce2d506a3f91c17177
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2019-01-16 16:37:17 +0600

    Install nano highlighter

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Created by Yevgeniy Goncharov, https://sys-adm.in
 # Install syntax highlight for nano editor
+# If you want install highlighter globally, please use /usr/share/nano/ folder
 
 # Envs
 # ---------------------------------------------------\
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
-# Install config highlight
-# ---------------------------------------------------\
-cp $SCRIPT_PATH/conf.nanorc /usr/share/nano/
-echo "include /usr/share/nano/conf.nanorc" >> ~/.nanorc
-
 # Install synatx highlight
 # ---------------------------------------------------\
 cp -r $SCRIPT_PATH/.nano ~/
 echo "include ~/.nano/syntax/ALL.nanorc" >> ~/.nanorc
+
+# Install config highlight
+# ---------------------------------------------------\
+echo "include ~/.nano/config/conf.nanorc" >> ~/.nanorc
